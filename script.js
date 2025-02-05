6a032b82cb2555285683b1d7ccc4b4a5d515a127
rice gallardo <gallr091@newschool.edu>
2025-02-04 22:14:33 -0500

    js needs fixing

---
+++ script.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const itemContainer = document.querySelector(".item-container");
+    const fakeWindowWrapper = document.getElementById("fake-window-wrapper");
+    const closeButton = document.getElementById("close-window");
+    const fakeWindow = document.getElementById("fake-window");
+
+    // Show window on click
+    itemContainer.addEventListener("click", function() {
+        fakeWindowWrapper.style.display = "flex";
+    });
+
+    // Close window
+    closeButton.addEventListener("click", function() {
+        fakeWindowWrapper.style.display = "none";
+    });
+
+    // Make the fake window draggable
+    dragElement(fakeWindow);
+});
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "-header")) {
+        document.getElementById(elmnt.id + "-header").onmousedown = dragMouseDown;
+    } else {
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
