6415708e904000025c94c8f733fed0656b1f52c8
rice gallardo <gallr091@newschool.edu>
2025-02-06 10:43:24 -0500

    added arena.js

---
+++ arena.js
@@ -0,0 +1,26 @@
+console.log('Hello, World!');
+
+let channelSlug = 'digital-brutalism-buorqfz9bag';
+
+etch(`https://api.are.na/v2/channels/${channelSlug}?per=100`, { cache: 'no-store' })
+	.then((response) => response.json()) // Return it as JSON data
+	.then((data) => { // Do stuff with the data
+		console.log(data) // Always good to check your response!
+		placeChannelInfo(data) // Pass the data to the first function
+
+		let placeChannelInfo = (data) => {
+		let title = document.querySelector('h1')
+		title.innerHTML = data.title
+		}
+		
+		// Loop through the `contents` array (list), backwards. Are.na returns them in reverse!
+		data.contents.reverse().forEach((block) => {
+			// console.log(block) // The data for a single block
+			renderBlock(block) // Pass the single block data to the render function
+		})
+
+		// Also display the owner and collaborators:
+		let channelUsers = document.getElementById('channel-users') // Show them together
+		data.collaborators.forEach((collaborator) => renderUser(collaborator, channelUsers))
+		renderUser(data.user, channelUsers)
+	})
